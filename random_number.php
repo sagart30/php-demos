626c7df136ee3e68b7921bc449b0b8211682d947
Sagar Thakur <SagarT@bom.tv18.com>
2019-07-29 15:54:59 +0530

    git : random number generation

---
+++ random_number.php
@@ -0,0 +1,20 @@
+<?php
+    //Array
+    $cars = array("Volvo", "BMW", "Audi", "Ferrari");
+    // $cars = array("Volvo");
+    $count = count($cars);
+
+    if ($count == 1) {
+        $randomnumber = str_pad(mt_rand(50 ,99999999), 8 ,substr(base_convert(sha1(uniqid(time())), 16, 36), 0, 8), STR_PAD_BOTH);
+        echo $randomnumber;
+        //echo "<br>String length: &nbsp;".strlen($randomnumber);
+    } 
+    elseif ($count > 1){
+        for($x=0;$x < $count;$x++){
+            $randomnumber = str_pad(mt_rand($x ,99999999), 8 ,substr(base_convert(sha1(uniqid(time())), 16, 36), 0, 8), STR_PAD_BOTH);
+            echo $randomnumber;
+            //echo "<br>String length: &nbsp;".strlen($randomnumber);
+            //echo "<br>";
+        }   
+    }
+?>
